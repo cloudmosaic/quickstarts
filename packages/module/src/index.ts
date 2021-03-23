ebc624fa6323b200492a4f17a7b01aa122a50d5e
Joachim Schuler <jschuler@redhat.com>
2021-03-23 16:34:24 -0400

    exports

--- packages/module/src/index.ts
+++ packages/module/src/index.ts
@@ -2,6 +2,7 @@ import './style.scss';
 import "./i18n/i18n";
 
 export * from './QuickStartCatalogPage';
+export * from './catalog'
 export * from './QuickStartDrawer';
 export * from './utils/quick-start-context';
 export * from './utils/quick-start-types';
