c54e1f6445f2f067ed4c52e7530bd851b9dd0ac7
Joachim Schuler <jschuler@redhat.com>
2021-03-23 15:37:53 -0400

    fixes

--- packages/dev/src/quickstarts-data/mocks/parsers/qs-adoc-parser.ts
+++ packages/dev/src/quickstarts-data/mocks/parsers/qs-adoc-parser.ts
@@ -4,7 +4,7 @@ import {
   QuickStartTask,
   QuickStartTaskReview,
   QuickStartTaskSummary,
-} from "@cloudmosaic/quickstarts/src/utils/quick-start-types";
+} from "@cloudmosaic/quickstarts";
 
 const processor = Processor();
 const asciiOptions = {
