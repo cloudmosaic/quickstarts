d2f058198d83da121ca7156ce2deb3fa044bff79
Joachim Schuler <jschuler@redhat.com>
2021-03-25 13:01:09 -0400

    prod build

--- packages/dev/webpack.config.js
+++ packages/dev/webpack.config.js
@@ -112,12 +112,8 @@ module.exports = (_env, argv) => {
         patterns: [{ from: "src/quickstarts-data/mas-guides/getting-started/images", to: "" }],
       }),
       new AssetsPlugin({
-        path: "public",
         keepInMemory: _env === "development",
         removeFullPathAutoPrefix: true
-      }),
-      new CopyPlugin({
-        patterns: [{ from: "public/webpack-assets.json", to: "" }],
       }),
       new CopyPlugin({
         patterns: [
